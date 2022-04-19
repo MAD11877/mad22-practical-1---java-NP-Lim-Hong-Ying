2cf7eba27f15a8ef353dfc94a07d2d702183c632
NP-Lim-Hong-Ying <103445741+NP-Lim-Hong-Ying@users.noreply.github.com>
2022-04-19 23:58:30 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter integer: ");
+    //System.out.print("Enter integer: ");
     int integer = in.nextInt();
     int mulint = integer * integer;
     System.out.println(mulint);
