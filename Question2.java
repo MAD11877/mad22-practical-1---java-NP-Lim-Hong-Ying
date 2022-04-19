55f94f9f4d6f4ed19a9f254d62e670fa86f9984a
NP-Lim-Hong-Ying <103445741+NP-Lim-Hong-Ying@users.noreply.github.com>
2022-04-19 23:57:26 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,14 +10,15 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
 
-    System.out.print("Enter your height: ");
+    //System.out.print("Enter your height: ");
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
 
-    System.out.print("Enter your weight: ");
+    //System.out.print("Enter your weight: ");
     double weight = in.nextDouble();
 
     double bmi = weight / Math.pow(height, 2);
-    System.out.println("Your BMI is : " + bmi);
+    //System.out.println("Your BMI is : " + bmi);
+    System.out.println(bmi);
   }
 }
